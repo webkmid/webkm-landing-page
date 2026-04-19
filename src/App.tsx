6b7bd3cd6dc7faa2027bfcb3b7f08a40610babb2
erzajuan <direkal2@gmail.com>
2026-04-19 12:17:09 +0700

    feat: implement WebKM component and restructure App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 // src/App.tsx
-import { Header } from './components/Header';
-import { Hero } from './components/Hero';
-import { Features } from './components/Features';
-import { Templates } from './components/Templates';
-import { Pricing } from './components/Pricing';
-import { Testimonials } from './components/Testimonials';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
+import  WebKM  from './components/WebKM';
 // import { InstagramFeed } from './components/InstagramFeed';
 
 export default function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <Templates />
-        <Pricing />
-        <Testimonials />
-        <Contact />
-        {/* <InstagramFeed /> */}
-      </main>
-      <Footer />
+      <WebKM/>
     </>
   );
 }
