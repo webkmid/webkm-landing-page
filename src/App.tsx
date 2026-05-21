66bfa9d6cbd36aa05f24d3847bd170f7c8734bb0
erzajuan <direkal2@gmail.com>
2026-05-21 14:02:29 +0700

    feat: add WebKM2 component with services, packages, testimonials, and portfolio sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // src/App.tsx
-import  WebKM  from './components/WebKM';
+// import  WebKM  from './components/WebKM';
+import WebKM2 from './components/webkm2';
 // import { InstagramFeed } from './components/InstagramFeed';
 
 export default function App() {
   return (
     <>
-      <WebKM/>
+      <WebKM2/>
     </>
   );
 }
